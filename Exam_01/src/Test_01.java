cf8c9f38423a021c5fec6268b8b3776962dab40e
websnet <websnet@websnet>
2025-02-11 16:17:08 +0900

    두번째 예제(25.02.12)

--- Exam_01/src/Test_01.java
+++ Exam_01/src/Test_01.java
@@ -2,6 +2,8 @@
 public class Test_01 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		//수정(추가)
+		System.out.println("Hello World");
 	}
 
 }
---
+++ Exam_01/src/Test_02.java
@@ -0,0 +1,9 @@
+
+public class Test_02 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World2 ");
+
+	}
+
+}
